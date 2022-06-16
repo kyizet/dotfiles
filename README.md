caa4bcaa71fd7d4d5061d5dd99a42dd6f5b289fb
kyizet <zet11zet@icloud.com>
2022-06-16 15:00:31 +0630

    Ganyu and Emilia

---
+++ README.md
@@ -0,0 +1,19 @@
+# Zet's Themes
+
+> **Disclaimer:** Just writing the README.md for the repository to look good
+
+## Overview
+| Category | Name |
+| ;-- | ;-- |
+| OS | Arch Linux |
+| WM | bspwm |
+| Bar | Polybar |
+| Terminal | Alacritty |
+| Compositor | Picom |
+| DMENU | rofi |
+
+## Themes List
+| Theme | Colors |
+| ;-- | ;-- |
+| Ganyu | dark blue |
+| Emilia | yellow-cyan |
---
+++ switch.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/bash
+
+# DIRS
+DIR=`pwd`
+DIR_bspwm="$HOME/.config/bspwm/"
+DIR_sxhkd="$HOME/.config/sxhkd/"
+DIR_polybar="$HOME/.config/polybar/"
+DIR_rofi="$HOME/.config/rofi/"
+DIR_picom="$HOME/.config/picom/"
+DIR_wallpaper="$HOME/Pictures/Wallpapers/"
+DIR_nmdmenu="$HOME/.config/networkmanager-dmenu"
+DIR_chrome="$HOME/.mozilla/firefox/*."
+
+# Vars
+themes=()
+set_theme=""
+chrome_profile=""
+
+# Switch themes
+switch_themes() {
+	echo "Hey"
+}
+
+cat << EOF
+/ __| _ __ __ (_)| |_  __ | |_        |_   _|| |_   ___  _ __   ___ 
+\__ \ \ V  V /| ||  _|/ _||   \         | |  |   \ / -_)| '  \ / -_)
+|___/  \_/\_/ |_| \__|\__||_||_|        |_|  |_||_|\___||_|_|_|\___|
+EOF
+echo "[+] Theme: "
+
+declare -i i=1
+for dir in *;
+	do 
+		if [[ -d $dir ]]; then
+			themes+=($dir)
+			echo "->[$i] ${dir^}";
+			i+=1;
+		fi
+	done
+
+echo ""
+read -p "[?] Choose Theme: " chosen_theme
+if [[ "$chosen_theme" =~ ^[0-9]+$ ]]
+	then
+		echo -e "[+] You chose ${themes[$chosen_theme-1]^}\n"
+		set_theme=${themes[$chosen_theme-1]}
+		cd $set_theme
+		read -p "[?] Chrome profile name: " chrome_profile
+else
+		echo -e "\n[-] Invalid Choice, Exiting\n"
+		fi
+
+if [[ -e . ]];
+then
+	echo -e "[+] Switching Themes\n"
+	for f in *;
+	do
+			case $f in
+				"bspwm")
+					echo -e "[+] Copying bspwm config"
+					cp -r $f/* "$DIR_bspwm"
+					echo -e "[+] Done\n"
+
+				;;
+				"picom")
+					echo -e "[+] Copying picom config"
+					cp -r $f/* "$DIR_picom"
+					echo -e "[+] Done\n"
+				;;
+				"polybar")
+					echo -e "[+] Copying polybar config"
+					cp -r $f/* "$DIR_polybar"
+					echo -e "[+] Done\n"
+				;;
+				"rofi")
+					echo -e "[+] Copying rofi config"
+					cp -r $f/* "$DIR_rofi" 
+					echo -e "[+] Done\n"
+				;;
+				"sxhkd")
+					echo -e "[+] Copying sxhkd config"
+					cp -r $f/* "$DIR_sxhkd"
+					echo -e "[+] Done\n"
+				;;
+				"Wallpapers")
+					echo -e "[+] Copying Wallpapers"
+					cp -r $f/* "$DIR_wallpaper"
+					echo -e "[+] Done\n"
+				;;
+				"networkmanager-dmenu")
+					echo -e "[+] Copying networkmanager-dmenu"
+					cp -r $f/* "$DIR_nmdmenu"
+					echo -e "[+] Done\n"
+				;;
+				"chrome")
+					echo -e "[+] Copying chrome profile"
+					cp -r $f/* "$DIR_chrome$chrome_profile"
+				;;
+			esac
+	done
+else
+	echo "[-] Empty theme"
+fi
